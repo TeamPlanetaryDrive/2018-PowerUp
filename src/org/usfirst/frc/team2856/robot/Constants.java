edb118d0b384bba04acce8ca3da598996f10568e
egor <egor.vladone@gmail.com>
2018-01-22 19:26:49 -0500

    fixed stupid formatting. TODO: find the person that originally formatted this code and learn them a thing or two

--- src/org/usfirst/frc/team2856/robot/Constants.java
+++ src/org/usfirst/frc/team2856/robot/Constants.java
@@ -10,21 +10,24 @@ public class Constants {
 	public static final double PERIODIC_UPDATE_PERIOD = 0.020; // Periodic update period (s)
 	
 	//Power Channels
-	public static final int DRIVE_POWER_LEFT_FRONT = 0,
-			DRIVE_POWER_RIGHT_FRONT = 1,
-			DRIVE_POWER_LEFT_REAR = 2,
-			DRIVE_POWER_RIGHT_REAR = 3,
-			CAMERA_POWER = 4,
-			VRM_POWER = 5,
-			INTAKE_POWER = 15;
+	public static final int 
+		DRIVE_POWER_LEFT_FRONT = 0,
+		DRIVE_POWER_RIGHT_FRONT = 1,
+		DRIVE_POWER_LEFT_REAR = 2,
+		DRIVE_POWER_RIGHT_REAR = 3,
+		CAMERA_POWER = 4,
+		VRM_POWER = 5,
+		INTAKE_POWER = 15;
 
 	//Digital (0-9, 10-25)
-	public static int LEFT_MOTOR_CHANNEL = 0,
-			RIGHT_MOTOR_CHANNEL = 1;
+	public static int 
+		LEFT_MOTOR_CHANNEL = 0,
+		RIGHT_MOTOR_CHANNEL = 1;
 
-	public static int SHOOTER_CHANNEL = 2,
-			HOPPER_CHANNEL = 3,
-			CLIMBER_CHANNEL = 4;
+	public static int 
+		SHOOTER_CHANNEL = 2,
+		HOPPER_CHANNEL = 3,
+		CLIMBER_CHANNEL = 4;
 	
 	public static int INTAKE_CHANNEL = 5;
 	
@@ -35,61 +38,64 @@ public class Constants {
 
 	//Analog Channels (0-3, 4-7)
 
-	public static int LEFT_ENC_CHANNEL_A = 0,
-			LEFT_ENC_CHANNEL_B = 1,
-			RIGHT_ENC_CHANNEL_A = 2,
-			RIGHT_ENC_CHANNEL_B = 3,
-			SHOOTER_ENC_CHANNEL_A = 4,
-			SHOOTER_ENC_CHANNEL_B = 5;
+	public static int 
+		LEFT_ENC_CHANNEL_A = 0,
+		LEFT_ENC_CHANNEL_B = 1,
+		RIGHT_ENC_CHANNEL_A = 2,
+		RIGHT_ENC_CHANNEL_B = 3,
+		SHOOTER_ENC_CHANNEL_A = 4,
+		SHOOTER_ENC_CHANNEL_B = 5;
 
 
-	public static int LEFT_JOYSTICK_PORT = 0,
-			RIGHT_JOYSTICK_PORT = 1;
+	public static int 
+		LEFT_JOYSTICK_PORT = 0,
+		RIGHT_JOYSTICK_PORT = 1;
 
 	public static double distancePerPulse = 0.003522;
 
-	public static final double
-	AUTO_DIST = 0.5;					// (feet) 15.0
+	public static final double AUTO_DIST = 0.5;					// (feet) 15.0
 
 	/**
 	 * SpeedController Stuff
 	 */
 
-	public static final boolean DRIVE_MOTOR_LEFT_AUTO_DIR = false,
-			DRIVE_MOTOR_LEFT_TELE_DIR = true,
-			DRIVE_MOTOR_RIGHT_AUTO_DIR = true,
-			DRIVE_MOTOR_RIGHT_TELE_DIR = true;
+	public static final boolean
+		DRIVE_MOTOR_LEFT_AUTO_DIR = false,
+		DRIVE_MOTOR_LEFT_TELE_DIR = true,
+		DRIVE_MOTOR_RIGHT_AUTO_DIR = true,
+		DRIVE_MOTOR_RIGHT_TELE_DIR = true;
 
-	public static final double DRIVE_ACCEL_RATE = 2.5,				// (ft/s^2) 5.0
-			DRIVE_ENCODER_RESOLUTION = 0.003522,// (feet/count) Real: 0.003568, Practice: 0.003522
-			DRIVE_GYRO_SENSITIVITY = 0.007,		// (volts/(degree/second)) 0.007
-			DRIVE_PID_EFFORT_MAX = 1.0,			// (0-1) 1.0
-			DRIVE_PID_PERIOD = 0.010,			// (s) 0.010
-			DRIVE_PID_POS_SETTLE = 0.25,		// (s) 0.25
-			DRIVE_SPEED_MAX = 2.5,				// (ft/s) 5.0
-			DRIVE_BASE_WIDTH = (23.0 / 12.0);
+	public static final double 
+		DRIVE_ACCEL_RATE = 2.5,				// (ft/s^2) 5.0
+		DRIVE_ENCODER_RESOLUTION = 0.003522,// (feet/count) Real: 0.003568, Practice: 0.003522
+		DRIVE_GYRO_SENSITIVITY = 0.007,		// (volts/(degree/second)) 0.007
+		DRIVE_PID_EFFORT_MAX = 1.0,			// (0-1) 1.0
+		DRIVE_PID_PERIOD = 0.010,			// (s) 0.010
+		DRIVE_PID_POS_SETTLE = 0.25,		// (s) 0.25
+		DRIVE_SPEED_MAX = 2.5,				// (ft/s) 5.0
+		DRIVE_BASE_WIDTH = (23.0 / 12.0);
 
 	public static final int DRIVE_ENC_SAMPLES_TO_AVERAGE = 4;
 
 	//Will add PWM Channels
 
 	//HARDWARE
-	public static Talon lMotor,
-	rMotor;
+	public static Talon lMotor, rMotor;
 
-	public static Talon shooter, 
-	hopper, 
-	climber,
-	intake;
+	public static Talon 
+		shooter, 
+		hopper, 
+		climber,
+		intake;
 
 	public static DigitalInput gearIn;
 
-	public static Encoder LEnc,
-	REnc,
-	SEnc;
+	public static Encoder 
+		LEnc,
+		REnc,
+		SEnc;
 
-	public static Joystick leftJoystick,
-	rightJoystick;
+	public static Joystick leftJoystick, rightJoystick;
 
 	public static Gyro gyro;
 
@@ -118,6 +124,7 @@ public class Constants {
 		gyro = new ADXRS450_Gyro();
 		gyro.calibrate();
 	}
+	//don't mind me
 	//just passing by
 
 }
