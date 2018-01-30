13c24e07e8bbea9cd6c95a0b4a05bcb42e0ad1fa
Alan Luo <Alan Luo@ALANLUO>
2018-01-29 19:43:04 -0500

    Made small, minor changes to LoopTele

--- src/org/usfirst/frc/team2856/robot/loop/LoopTele.java
+++ src/org/usfirst/frc/team2856/robot/loop/LoopTele.java
@@ -9,7 +9,6 @@ public class LoopTele extends Loop{
 	
 	public LoopTele(Robot rob){super(rob);}
 	
-	
 	private double leftY = Constants.leftJoystick.getY();
 	private double rightY = Constants.rightJoystick.getY();
 		
@@ -17,31 +16,26 @@ public class LoopTele extends Loop{
 	public void init() {
 		robot.driveTrain.initTele();
 	}
-
-	
-	
-	
 	
 	public void loop() {
-		if(!Constants.button3_left.get() && !Constants.button3_right.get()) {
-			leftY = Constants.leftJoystick.getY();
-			rightY = Constants.rightJoystick.getY();
-		}
+		leftY = Constants.leftJoystick.getY();
+		rightY = Constants.rightJoystick.getY();
 		
 		if (Constants.leftJoystick.getTrigger()){
 			leftY /= 3;
 			rightY /= 3;
 		}
+		
 		if(Constants.button3_left.get() || Constants.button3_right.get()){
 			double maxY = Math.max(leftY, rightY);
 			leftY = maxY;
 			rightY = maxY;
 		}
+		
 		robot.driveTrain.tankDrive(leftY, rightY);
 		//robot.shooter.updateTele();
 		//robot.climber.updateTele();
 		//robot.intake.updateTele();
-		
 	}
 
 }
